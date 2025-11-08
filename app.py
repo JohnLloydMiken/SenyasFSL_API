f391bd717096e81cd8eeb78b6611dc35bcfea6a8
John Lloyd H. Miken <mikenjohnlloyd@gmail.com>
2025-11-08 23:24:12 +0800

    Replace numeric strings with word representations

--- app.py
+++ app.py
@@ -23,11 +23,11 @@ label_sets = {
         "K", "L", "M", "N", "O", "P", "Q", "R", "S",
         "T", "U", "V", "W", "X", "Y", "J", "Ñ", "NG", "Z"
     ],
-    "numbers": [
-        "1", "2", "3", "4", "5", "6", "7", "8", "9",
-        "10", "11", "12", "13", "14", "15", "16", "17",
-        "18", "19", "20", "21"
-    ],
+  "numbers": [
+    "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+    "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen",
+    "Seventeen", "Eighteen", "Nineteen", "Twenty", "Twenty-One"
+],
     "ordinals": [
         "1st", "2nd", "3rd", "4th", "5th",
         "6th", "7th", "8th", "9th", "10th"
