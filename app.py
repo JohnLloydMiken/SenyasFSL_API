925e8469da377094c85cf66f2fac4dc7f27681bb
John Lloyd Miken <mikenjohnlloyd@gmail.com>
2025-11-23 21:01:03 +0800

    Fixes the labels

--- app.py
+++ app.py
@@ -68,8 +68,8 @@ label_sets = {
         "Past", "September", "Tomorrow", "Year", "Yesterday"
     ],
     "time_weather": [
-        "10:00 AM", "10:00 PM", "11: 00 AM", "11:00 PM", "12:00 AM",
-        "12:00 PM", "1:00 PMm", "2:00 PM", "3:00 PM", "4:00 PM",
+        "10:00 AM", "10:00 PM", "11:00 AM", "11:00 PM", "12:00 AM",
+        "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM",
         "5:00 PM", "6:00 AM", "6:00 PM", "7:00 AM", "7:00 PM",
         "8:00 AM", "8:00 PM", "9:00 AM", "9:00 PM",
         "Bright", "Clear sky", "Cloud", "Cold",
